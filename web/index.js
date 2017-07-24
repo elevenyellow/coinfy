8dabb6edde41446276b5f5cdca9561b3e8d7eee0
Josenzo <enzo@josema.es>
2017-07-24 12:42:51 +0200

    Better setup webpack + express

--- web/index.js
+++ web/index.js
@@ -1,21 +1,9 @@
 const express = require('express');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack/development.js')();
-const createWebpackMiddleware = require('webpack-dev-middleware');
-const createWebpackHotMiddleware = require('webpack-hot-middleware');
 const path = require('path');
+const webpack = require('./webpack/development')
+const app = express()
 
-const compiler = webpack(webpackConfig);
-const app = express();
-app.use('/', express.static(path.resolve(__dirname, './dist')));
-const webpackDevMiddleware = createWebpackMiddleware(compiler, {
-  quiet: true,
-  noInfo: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-  publicPath: '/app',
-});
-app.use(webpackDevMiddleware);
-app.use(createWebpackHotMiddleware(compiler));
-app.listen(8001, () => console.log('App listening on http://localhost:8001'));
+
+webpack(app)
+app.use('/', express.static(path.resolve(__dirname, './dist')))
+app.listen(8001, () => console.log('App listening on http://localhost:8001'))
--- web/webpack/development.js
+++ web/webpack/development.js
@@ -1,43 +1,58 @@
 const webpack = require('webpack');
+const createWebpackMiddleware = require('webpack-dev-middleware');
+const createWebpackHotMiddleware = require('webpack-hot-middleware');
 const path = require('path');
 
-function configFactory() {
-  return {
-    devtool: 'inline-source-map',
-    entry: {
-      main: [
-        'webpack-hot-middleware/client',
-        path.resolve(__dirname, '../client/index.js')
-      ],
+const config = {
+  devtool: 'inline-source-map',
+  entry: {
+    main: [
+      'webpack-hot-middleware/client',
+      path.resolve(__dirname, '../client/index.js')
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: '[name].js',
+    chunkFilename: '[name]-[chunkhash].js',
+    publicPath: '/app',
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat',
     },
-    output: {
-      path: path.resolve(__dirname, '../build'),
-      filename: '[name].js',
-      chunkFilename: '[name]-[chunkhash].js',
-      publicPath: '/app',
-    },
-    resolve: {
-      extensions: ['.js'],
-      alias: {
-        'react': 'preact-compat',
-        'react-dom': 'preact-compat',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/]
       },
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: [/node_modules/]
-        },
-      ],
-    },
-    plugins: [
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin()
-    ]
-  }
+    ],
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 }
 
-module.exports = configFactory;
+const compiler = webpack(config);
+
+const webpackDevMiddleware = createWebpackMiddleware(compiler, {
+  quiet: true,
+  noInfo: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+  publicPath: '/app',
+});
+
+
+module.exports = function(app) {
+  app.use(webpackDevMiddleware);
+  app.use(createWebpackHotMiddleware(compiler));
+}
