d0751dbaa0fb35d81abbb18be266e94b495f61a3
Josenzo <enzo@josema.es>
2017-07-26 16:59:37 +0200

    Left side layout done

--- web/client/components/App.js
+++ web/client/components/App.js
@@ -55,7 +55,7 @@ export default function App() {
                         </ColumnLeftHeader>
                         <ColumnLeftContent>
                             {[0,0,0,0,0,0,0].map((e,index)=>(
-                            <Wallet>
+                            <Wallet selected={index===2}>
                                 <WalletIcon>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32" class="currencyIcons-cEnxSc jkLgUs"><g fill="none" fill-rule="evenodd"><circle cx="16" cy="16" r="16" fill="#FFB119"></circle><path fill="#FFF" d="M21.78 15.37c.51-.61.83-1.4.83-2.26 0-2.74-1.6-4.38-4.24-4.38V5.45c0-.12-.1-.22-.22-.22h-1.27c-.11 0-.2.1-.2.21v3.3h-1.7V5.44c0-.12-.1-.22-.22-.22H13.5c-.12 0-.2.1-.21.21v3.3H9.67c-.12 0-.21.09-.21.21v1.31c0 .12.1.22.21.22h.21c.94 0 1.7.79 1.7 1.75v7c0 .92-.68 1.67-1.55 1.75a.21.21 0 0 0-.18.16l-.33 1.32c-.01.06 0 .13.04.19.04.05.1.08.17.08h3.55v3.3c0 .1.1.2.2.2h1.28c.12 0 .21-.1.21-.22v-3.28h1.7v3.3c0 .1.1.2.21.2h1.27c.12 0 .22-.1.22-.22v-3.28h.85c2.65 0 4.24-1.64 4.24-4.37 0-1.28-.68-2.39-1.68-3zm-6.8-4.01h2.54c.94 0 1.7.78 1.7 1.75 0 .96-.76 1.75-1.7 1.75h-2.55v-3.5zm3.39 8.75h-3.4v-3.5h3.4c.93 0 1.7.78 1.7 1.75 0 .96-.77 1.75-1.7 1.75z"></path></g></svg>
                                 </WalletIcon>
@@ -218,11 +218,11 @@ position: absolute;
 width: 100%;
 top: 215px;
 &::-webkit-scrollbar {
-  width: 6px;
-  height: 6px;
+  width: 8px;
+  height: 8px;
 }
 &::-webkit-scrollbar-thumb {
-  background: rgba(0, 0, 0, 0.07);
+  background: ${styles.color.background};
   border-radius: 1em;
   cursor: grab;
 }
@@ -243,7 +243,13 @@ padding: 10px;
 
 const Wallet = styled.div`
 padding: 10px 15px;
-border-bottom:2px solid ${styles.color.background}
+border-bottom:2px solid ${styles.color.background};
+${props=>{
+    if (props.selected) {
+        return `background: linear-gradient(90deg, ${styles.color.background}, #e9eef1)`
+    }
+}}
+
 `
 const WalletIcon = styled.div`
 float:left;
@@ -254,15 +260,17 @@ margin-left: 37px;
 `
 const WalletLabel = styled.div`
 font-weight: bold;
-font-size: 18px;
+font-size: 17px;
 color: ${styles.color.front3};
-line-height: 20px;
+line-height: 17px;
+letter-spacing: 0.3px;
 `
 const WalletBalance = styled.div`
 font-size: 11px;
 color: ${styles.color.front2};
 padding-top: 3px;
 font-weight: 100;
+letter-spacing: 0.3px;
 `
 
 
