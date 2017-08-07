edf16e3476e00e8fe95b5104095aca0fa420e4cb
Josenzo <enzo@josema.es>
2017-08-07 18:09:02 +0200

    Making router

--- web/src/client/router.js
+++ web/src/client/router.js
@@ -1,17 +1,34 @@
 
 function parse(url) {
-    var data = {},
-        resu = /(.*):\/\/([^/#?]+)([^?#]*)([^#]*)(.*)?/.exec(url)
-    
-    data.href = url
-    data.protocol = resu[1]
-    data.host = resu[2]
-    data.pathname = resu[3]
-    data.search = resu[4]
-    data.hash = resu[5] || ''
+    var resu = /(.*):\/\/([^/#?]+)([^?#]*)([^#]*)(.*)?/.exec(url)
+    var query = {}
+    var data = {
+        href: url,
+        protocol: resu[1],
+        host: resu[2],
+        pathname: resu[3],
+        path: resu[3].split('/').filter(function (item) { return item.length>0 }),
+        search: resu[4],
+        query: query,
+        hash: resu[5] || ''
+    }
+
+    if (data.search.length > 1) {
+        data.search.substr(1).split('&').forEach(function(item) {
+            if (item.length > 0) {
+                var equal = item.indexOf('=')
+                if (equal > -1)
+                    data.query[item.substr(0,equal)] = item.substr(equal+1)
+                else
+                    data.query[item] = ''
+            }
+        })
+    }
+
+
     return data
 }
-console.log(   parse('http://localhost:8000?#dafs=daf') )
+console.log(   parse('http://localhost:8000/wendios/peri/?A=123&B&=C&#molando') )
 
 
 
