1ff45a6521e2c449b6fcc7f2418b6896b211e25a
Josenzo <enzo@josema.es>
2017-10-03 09:39:19 +0200

    Renaming folder

---
+++ web/src/api/Assets/index.js
@@ -0,0 +1,17 @@
+import * as btc from '/api/Assets/BTC'
+import * as bch from '/api/Assets/BCH'
+
+
+export const Assets = {
+    BTC: btc,
+    // BCH: bch,
+}
+
+export const BTC = btc
+export const BCH = bch
+
+
+
+export function getAssetId({symbol, address, type='wallet'}) {
+    return `${symbol}-${address}`
+}
