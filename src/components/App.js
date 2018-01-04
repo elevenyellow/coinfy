115aca53c3eb2abd41253f66f17138b0a7416587
Josenzo <enzo@josema.es>
2018-01-04 12:32:40 +0100

    Added version and link to repository in footer

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,7 @@ import Notifications from '/components/partials/Notifications'
 import Header from '/components/partials/Header'
 import SideMenu from '/components/partials/SideMenu'
 import Right from '/components/partials/Right'
-// import Footer from '/components/partials/Footer'
-
+import Footer from '/components/partials/Footer'
 
 export default function App() {
     return (
@@ -18,30 +17,29 @@ export default function App() {
             <Content>
                 <Right />
             </Content>
-            {/* <Footer /> */}
+            <Footer />
             <Popups />
         </Background>
     )
 }
 
-
-
-
 const Background = styled.div`
-height:100%;
-background: linear-gradient(to bottom, #007196 150px,#d7dbd5 150px);
+    height: 100%;
+    background: linear-gradient(to bottom, #007196 150px, #d7dbd5 150px);
 `
 
 const Content = styled.div`
-height: calc(100% - ${styles.headerHeight} - ${styles.paddingOut});
-margin: 0 ${styles.paddingOut};
-position: relative;
-background: white;
-box-shadow: 0 1px 1px 0 rgba(0,0,0,0.06), 0 2px 5px 0 rgba(0,0,0,0.2);
-border-radius: 3px;
+    height: calc(100% - ${styles.headerHeight} - ${styles.paddingOut});
+    margin: 0 ${styles.paddingOut};
+    position: relative;
+    background: white;
+    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.06), 0 2px 5px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 3px;
 
-${styles.media.second} {
-margin: 0 ${styles.paddingOutMobile};
-height: calc(100% - ${styles.headerHeight} - ${styles.paddingOutMobile});
-}
+    ${styles.media.second} {
+        margin: 0 ${styles.paddingOutMobile};
+        height: calc(
+            100% - ${styles.headerHeight} - ${styles.paddingOutMobile}
+        );
+    }
 `
--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import styles from '/const/styles'
+import { version, repository } from './../../../package.json'
 
 export default function Footer() {
     return (
         <FooterDiv>
+            <div>
+                <a href={repository} target="_blank">
+                    {' '}
+                    Version: {version}
+                </a>
+            </div>
         </FooterDiv>
     )
 }
 
-
-
 const FooterDiv = styled.div`
-height: ${styles.paddingOut};
-padding: 0 ${styles.paddingOut};
+    /* height: ${styles.paddingOut}; */
+    padding: 0 ${styles.paddingOut};
+    ${styles.media.second} {
+        div {
+            display: none;
+        }
+    }
+    div {
+        padding-top: 15px;
+    }
+    a {
+        font-size: 12px;
+        color: #000;
+        font-weight: bold;
+        letter-spacing: 0.2px;
+        display: block;
+        padding-left: 10px;
+        opacity: 0.3;
+    }
+    a:hover {
+        opacity: 0.5;
+    }
+
 `
