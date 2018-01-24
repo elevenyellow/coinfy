4e7e2f82e17954f75ed494bba1a78c911a26258e
Josenzo <enzo@josema.es>
2018-01-24 18:51:08 +0100

    Implemented basic example of bip39/44/32

--- miscellaneous/experiments/bip39.js
+++ miscellaneous/experiments/bip39.js
@@ -1,18 +1,35 @@
+// https://iancoleman.io/bip39/
+// https://github.com/ethereum/EIPs/issues/84
+// https://github.com/trapp/ethereum-bip44
+// m/44'/60'/0'/0/x: BIP44, MetaMask, Jaxx, MyEtherWallet (default), TREZOR App, Exodus
+// m/44'/60'/x'/0/0: BIP44, KeepKey, MetaMask (custom)
+// m/44'/60'/0'/x: Electrum, MyEtherWallet (ledger), Ledger Chrome App, imToken
+// m/44'/coin_type'/account'/0: Coinomi
+
 var bip39 = require('bip39') // npm i -S bip39
 var crypto = require('crypto')
 var bitcoin = require('bitcoinjs-lib') // npm i -S bitcoinjs-lib
+var ethereum = require('ethereumjs-util')
 
-var  randomBytes = crypto.randomBytes(16) // 128 bits is enough
-// var mnemonic = bip39.entropyToMnemonic(randomBytes.toString('hex')) 
-var mnemonic = 'report useless hybrid carry fragile acid never between accuse sunset transfer slow skill autumn lyrics'
-var seed = bip39.mnemonicToSeed(mnemonic) // you'll use this in #3 below
+var network = bitcoin.networks.bitcoin
 
+// var randomBytes = crypto.randomBytes(16) // 128 bits is enough
+// var words = bip39.entropyToMnemonic(randomBytes.toString('hex'))
+var words =
+    'cycle ladder vault piano steel put copy cancel purse scare before wood'
+var seed = bip39.mnemonicToSeed(words, '')
 
+var bip32RootKey = bitcoin.HDNode.fromSeedHex(seed, network)
+// var bip32RootKey = bitcoin.HDNode.fromSeedBuffer(seed, network)
+// var bip32RootKey = bitcoin.HDNode.fromBase58(rootKeyBase58, network)
 
-var bitcoinNetwork = bitcoin.networks.bitcoin
-var hdMaster = bitcoin.HDNode.fromSeedBuffer(seed, bitcoinNetwork) // seed from above
-var key1 = hdMaster.derivePath("m/44'/0'/0'/0/0")
-var key2 = hdMaster.derivePath("m/44'/0'/0'/0/1")
+var keyBTC = bip32RootKey.derivePath("m/44'/0'/0'/0/0")
+var keyETH = bip32RootKey.derivePath("m/44'/60'/0'/0/0")
 
-console.log(key1.keyPair.toWIF())
-console.log(key2.keyPair.toWIF())
+// .toWIF()
+console.log(seed.toString('hex'))
+console.log(bip32RootKey.toBase58())
+console.log(keyBTC.keyPair.getAddress())
+console.log(
+    ethereum.privateToAddress(keyETH.keyPair.d.toBuffer()).toString('hex')
+)
