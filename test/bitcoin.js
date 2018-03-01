50b9e0a64f4fa6e3f4711cdf3d7e75977249223a
Josenzo <enzo@josema.es>
2018-03-01 17:44:14 +0100

    Small setup for tests

--- test/bitcoin.js
+++
@@ -1,8 +0,0 @@
-const test = require('tape')
-const Bitcoin = require('../src/api/Coins/BTC')
-
-test('Array: unshift 2', t => {
-    t.deepEqual(1, 1)
-
-    t.end()
-})
---
+++ test/numbers.js
@@ -0,0 +1,7 @@
+import test from 'tape'
+import { decimals, bigNumber } from '/api/numbers'
+
+test('decimals, decimalsMax', t => {
+    t.equal(1, 1)
+    t.end()
+})
