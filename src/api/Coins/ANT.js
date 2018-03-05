ec993c465d01fed7d30047d5493a1b6a65d09a94
Josenzo <enzo@josema.es>
2018-03-05 17:35:13 +0100

    Making ERC20 as dynamic import

--- src/api/Coins/ANT.js
+++ src/api/Coins/ANT.js
@@ -1,43 +1,23 @@
-import { formatCoin } from '/api/numbers'
-import {
-    fetchBalance as fetchBalanceRaw,
-    fetchSummary as fetchSummaryRaw,
-    fetchTxs as fetchTxsRaw,
-    createSimpleTxRaw,
-    urlInfoRaw
-} from '/api/Coins/ERC20'
+import { createERC20 } from './ERC20_2'
 
-export * from './ERC20'
+console.log(
+    createERC20({
+        symbol: 'ANT',
+        name: 'Aragon',
+        color: '#2cdee1',
+        contract_address: '0x960b236a07cf122663c4303350609a66a7b288c0',
+        labels: 'ant ethereum token erc20 ecr20',
+        coin_decimals: 18,
+        price_decimals: 2
+    })
+)
 
-export const symbol = 'ANT'
-export const name = 'Aragon'
-export const color = '#2cdee1'
-export const contract_address = '0x960b236a07cf122663c4303350609a66a7b288c0'
-export const coin_decimals = 18
-export const price_decimals = 2
-export const satoshis = Math.pow(10, coin_decimals)
-export const labels = 'ant ethereum token erc20 ecr20'
-
-export function urlInfo(address) {
-    return urlInfoRaw(address, contract_address)
-}
-
-export function format(value, decimals = coin_decimals) {
-    return formatCoin(value, decimals, symbol)
-}
-
-export function fetchBalance(address) {
-    return fetchBalanceRaw(address, contract_address, satoshis)
-}
-
-export function fetchSummary(address) {
-    return fetchSummaryRaw(address, contract_address, satoshis)
-}
-
-export function fetchTxs(address, from, to) {
-    return fetchTxsRaw(address, from, to, contract_address, satoshis)
-}
-
-export function createSimpleTx(params) {
-    return createSimpleTxRaw(params, contract_address, coin_decimals)
-}
+export default createERC20({
+    symbol: 'ANT',
+    name: 'Aragon',
+    color: '#2cdee1',
+    contract_address: '0x960b236a07cf122663c4303350609a66a7b288c0',
+    labels: 'ant ethereum token erc20 ecr20',
+    coin_decimals: 18,
+    price_decimals: 2
+})
