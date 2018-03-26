8cc2f6f3357a6783d0472ec7bbca06a288535c45
Josenzo <enzo@josema.es>
2018-03-26 12:15:30 +0200

    Testing with bip44 to derive segwit address

--- miscellaneous/experiments/bip39.js
+++ miscellaneous/experiments/bip39.js
@@ -11,26 +11,64 @@ var crypto = require('crypto')
 var bitcoin = require('bitcoinjs-lib') // npm i -S bitcoinjs-lib
 var ethereum = require('ethereumjs-util')
 
-var network = bitcoin.networks.bitcoin
+function getSegwitAddressFromPrivateKey(private_key, network) {
+    const wallet = bitcoin.ECPair.fromWIF(private_key, network)
+    // wallet.compressed = false
+    const nkeyp = new bitcoin.ECPair(wallet.d)
+    return getSegwitAddressFromECPair(nkeyp)
+}
+
+function getSegwitAddressFromECPair(nkeyp, network) {
+    const pubKey = nkeyp.getPublicKeyBuffer()
+    const pubKeyHash = bitcoin.crypto.hash160(pubKey)
+    const redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(
+        pubKeyHash
+    )
+    const redeemScriptHash = bitcoin.crypto.hash160(redeemScript)
+    const scriptPubKey = bitcoin.script.scriptHash.output.encode(
+        redeemScriptHash
+    )
+    return bitcoin.address.fromOutputScript(scriptPubKey, network)
+}
+
+const mainnet = bitcoin.networks.mainnet
+const testnet = bitcoin.networks.testnet
 
 // var randomBytes = crypto.randomBytes(16) // 128 bits is enough
 // var words = bip39.entropyToMnemonic(randomBytes.toString('hex'))
 var words =
     'cycle ladder vault piano steel put copy cancel purse scare before wood'
 var seed = bip39.mnemonicToSeed(words, '')
+// console.log('seed:', seed.toString('hex'))
 
-var bip32RootKey = bitcoin.HDNode.fromSeedHex(seed, network)
-// var bip32RootKey = bitcoin.HDNode.fromSeedBuffer(seed, network)
-// var bip32RootKey = bitcoin.HDNode.fromBase58(rootKeyBase58, network)
+var bip32RootKey = bitcoin.HDNode.fromSeedHex(seed, mainnet)
+var bip32RootKeyTestnet = bitcoin.HDNode.fromSeedHex(seed, testnet)
+// // var bip32RootKey = bitcoin.HDNode.fromSeedBuffer(seed, mainnet)
+// // var bip32RootKey = bitcoin.HDNode.fromBase58(rootKeyBase58, mainnet)
+// console.log('bip32RootKey:', bip32RootKey.toBase58())
+// console.log('bip32RootKeyTestnet:', bip32RootKeyTestnet.toBase58())
 
 var keyBTC = bip32RootKey.derivePath("m/44'/0'/0'/0/0")
+var keyBTCsegwit = bip32RootKey.derivePath("m/49'/0'/0'/0/0")
+var keyBTCTestnet = bip32RootKeyTestnet.derivePath("m/44'/1'/0'/0/0")
+var keyBTCsegwitTestnet = bip32RootKeyTestnet.derivePath("m/49'/1'/0'/0/0")
 var keyETH = bip32RootKey.derivePath("m/44'/60'/0'/0/0")
 
-// .toWIF()
-console.log('seed:', seed.toString('hex'))
-console.log('bip32RootKey:', bip32RootKey.toBase58())
-console.log(keyBTC.keyPair.getAddress())
+// //
+console.log('BTC: ', keyBTC.keyPair.getAddress()) // .toWIF()
 console.log(
+    'BTCsegwit: ',
+    getSegwitAddressFromECPair(keyBTCsegwit.keyPair, mainnet)
+) // .toWIF()
+console.log('BTCTestnet: ', keyBTCTestnet.keyPair.getAddress()) // .toWIF()
+console.log(
+    'BTCsegwitTestnet: ',
+    getSegwitAddressFromECPair(keyBTCsegwitTestnet.keyPair, testnet)
+) // .toWIF()
+
+// ETH
+console.log(
+    'ETH: ',
     '0x' +
         ethereum.privateToAddress(keyETH.keyPair.d.toBuffer()).toString('hex')
 )
