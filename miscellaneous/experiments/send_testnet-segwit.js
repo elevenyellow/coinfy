66abbf17f0d478d12681b09d679d4aa45651cb21
Josenzo <enzo@josema.es>
2018-03-28 19:17:38 +0200

    Example send tx with segwit and regular address

--- miscellaneous/experiments/send_testnet-segwit.js
+++ miscellaneous/experiments/send_testnet-segwit.js
@@ -1,74 +1,50 @@
 // ------ script -------
 const request = require('request')
 const Bitcoin = require('bitcoinjs-lib')
-// https://testnet.coinfaucet.eu/en/
-// http://tpfaucet.appspot.com/
-// https://testnet.manu.backend.hamburg/faucet
 
 const network = Bitcoin.networks.testnet
-
-const alice = 'cQA29xw1do9iux5ZHRbRNVP81EPExK3N2Z6sgMKZsqd7NbH3uTec'
-const bob = 'cRhAysbHYkydyzyA2fo9TDELwFiDmRjA1eXv7itSHiQPvUG3iBQi'
-const aliceaddr = getSegwitAddressFromPrivateKey(alice) // 2N9Cki8ABz6oXuoF24rd5eAFPwwVpEWYctt
-const bobaddr = getSegwitAddressFromPrivateKey(bob) // 2NDcpwiwbrXxy3JEow32TfixJEuWFLvDzLw
-const fee = 10000
-const addrs = {
-    [aliceaddr]: alice,
-    [bobaddr]: bob
+const fee = 100000
+const alice = 'cQA29xw1do9iux5ZHRbRNVP81EPExK3N2Z6sgMKZsqd7NbH3uTec' // 2N9Cki8ABz6oXuoF24rd5eAFPwwVpEWYctt
+const bob = 'cRhAysbHYkydyzyA2fo9TDELwFiDmRjA1eXv7itSHiQPvUG3iBQi' // 2NDcpwiwbrXxy3JEow32TfixJEuWFLvDzLw
+const connor = '93KjsqW9w3efffxS4jb9AefrRoXwFx62UZumWwnkgX6mLYMPZgx' // mwANXKvZEnDyRRY4dWwuvCkrhMCfcxruQg
+const keys = {
+    [getSegwitAddressFromPrivateKey(alice)]: alice,
+    [getSegwitAddressFromPrivateKey(bob)]: bob,
+    [getAddressFromPrivateKey(connor)]: connor
 }
-
-// request(
-//     `https://test-insight.bitpay.com/api/addr/${aliceaddr}`,
-//     (error, response, body) => {
-//         if (!error && response.statusCode === 200) {
-//             const data = JSON.parse(body)
-//             const balance =
-//                 data.unconfirmedBalanceSat < 0
-//                     ? data.balanceSat + data.unconfirmedBalanceSat
-//                     : data.balanceSat
+const addrs = Object.keys(keys)
 
 request(
-    `https://test-insight.bitpay.com/api/addrs/${Object.keys(
-        addrs
-    ).join()}/utxo`,
+    `https://test-insight.bitpay.com/api/addrs/${addrs.join()}/utxo`,
     (error, response, body) => {
         if (!error && response.statusCode === 200) {
             const txs = JSON.parse(body)
             const balance = txs.map(tx => tx.satoshis).reduce((a, c) => a + c)
 
             const balance_available = balance - fee
-            const amount = balance_available / 2
-            const amount_back = balance_available / 2
+            const amount = Math.round(balance_available / addrs.length)
             console.log('balance', fromSatoshis(balance))
             console.log('fee', fromSatoshis(fee))
             console.log('balance_available', fromSatoshis(balance_available))
             console.log('amount', fromSatoshis(amount))
-            console.log('amount_back', fromSatoshis(amount_back))
 
             // Adding inputs
             const txb = new Bitcoin.TransactionBuilder(network)
             txs.forEach(tx => txb.addInput(tx.txid, tx.vout))
 
             // Adding outputs
-            txb.addOutput(bobaddr, amount)
-            txb.addOutput(aliceaddr, amount_back)
+            addrs.forEach(addr => txb.addOutput(addr, amount))
 
             // Signing txs
             txb.inputs.forEach((input, index) => {
                 // Signing
                 const tx = txs[index]
-                const private_key_ecpair = Bitcoin.ECPair.fromWIF(
-                    addrs[tx.address],
-                    network
-                )
-                const redeem_script = getRedeemScript(private_key_ecpair)
-                txb.sign(
-                    index,
-                    private_key_ecpair,
-                    redeem_script,
-                    null,
-                    tx.satoshis
-                )
+                const addr = tx.address
+                const ecpair = Bitcoin.ECPair.fromWIF(keys[addr], network)
+                const redeem_script = getRedeemScript(ecpair)
+                isSegwitAddress(addr)
+                    ? txb.sign(index, ecpair, redeem_script, null, tx.satoshis)
+                    : txb.sign(index, ecpair)
             })
 
             const transaction = txb.build()
@@ -82,11 +58,19 @@ request(
     }
 )
 
+// https://testnet.coinfaucet.eu/en/
+// http://tpfaucet.appspot.com/
+// https://testnet.manu.backend.hamburg/faucet
 // https://bitcoinfees.21.co/api/v1/fees/recommended
 // https://insight.bitpay.com/api/utils/estimatefee
 
 function fromSatoshis(n) {
     return n / 100000000
+}
+
+function getAddressFromPrivateKey(private_key) {
+    const wallet = Bitcoin.ECPair.fromWIF(private_key, network)
+    return wallet.getAddress().toString()
 }
 
 function getSegwitAddressFromPrivateKey(private_key) {
