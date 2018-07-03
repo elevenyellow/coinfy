344460754d1017d81932ea412459638f74e158d9
Josenzo <enzo@josema.es>
2018-07-03 19:27:46 +0200

    Hashes_github script with prompt inputs instead of parameters

--- scripts/hashes_github.js
+++ scripts/hashes_github.js
@@ -9,107 +9,116 @@ const respository = `https://api.github.com/repos/elevenyellow/coinfy/git/trees/
 const extensions = ['js', 'htm', 'html', 'css']
 
 // Getting email
-const email = process.argv[2]
-const password = process.argv[3]
-const timeout_repeat = process.argv[4] | 1
-const host = process.argv[5] | 'smtp.gmail.com'
-
+let timeout_repeat = 1
+let host = 'smtp.gmail.com'
 let server
 let repeat = false
 
-// Getting parameters
-if (typeof email == 'string') {
-    if (validateEmail(email)) {
-        if (typeof password == 'string') {
-            repeat = true
-            server = emailjs.server.connect({
-                user: email,
-                password: password,
-                host: host,
-                ssl: true
-            })
-        } else {
-            console.log('Type a password as second parameter')
-            process.exit()
+prompt('Email (optional): ', email => {
+    if (typeof email == 'string' && email.length > 0) {
+        if (!validateEmail(email)) {
+            console.log(colors.red('Invalid email'))
+            return process.exit()
         }
-    } else {
-        console.log('Invalid email')
-        process.exit()
-    }
-}
 
-console.log('Getting tree list...')
-getTree(respository, file => file.path.indexOf('public') > -1).then(list => {
-    checkFiles(list)
-})
-
-function checkFiles(list) {
-    console.log('Checking list: ' + new Date().toString())
-    let completed = 0
-    let incorrect = []
-    list = list.filter(item => extensions.includes(getExtension(item.path)))
-    list.forEach(item => {
-        const path = getPath(item)
-        getFile(path).then(body => {
-            completed += 1
-            const hash_github = item.sha
-            const hash_domain = shagit(body)
-            if (hash_domain === hash_github) {
-                console.log(colors.green(`✔ ${hash_github} `) + path)
+        prompt('Password: ', password => {
+            if (typeof password == 'string' && password.length > 0) {
+                repeat = true
+                server = emailjs.server.connect({
+                    user: email,
+                    password: password,
+                    host: host,
+                    ssl: true
+                })
+                init(email)
             } else {
-                incorrect.push(item)
-                console.log(
-                    colors.red(`✘ ${hash_domain}`) +
-                        ` ${path}\n  ` +
-                        colors.red(hash_github)
-                )
-            }
-
-            if (completed === list.length) {
-                const total_incorrects = incorrect.length
-                const total_list = list.length
-                console.log(
-                    colors[total_incorrects === 0 ? 'green' : 'red'].bold(
-                        `  ${total_incorrects} fails`
-                    )
-                )
-
-                if (repeat) {
-                    if (total_incorrects > 0) {
-                        console.log('')
-                        console.log('Sending email...')
-                        const subject = incorrect
-                            .map(item => `${item.sha} ${getPath(item)}`)
-                            .join('\n')
-
-                        server.send(
-                            {
-                                text: 'I hope this works',
-                                subject:
-                                    'Those files does no match with the github version:\n' +
-                                    subject,
-                                from: `you <${email}>`,
-                                to: `you <${email}>`
-                                // cc:      "else <else@your-email.com>",
-                            },
-                            function(err, message) {
-                                if (err) {
-                                    console.log(colors.red(err.message))
-                                    console.log('')
-                                } else console.log(colors.green('Email sent!'))
-                            }
-                        )
-                    }
-
-                    setTimeout(() => {
-                        checkFiles(list)
-                    }, timeout_repeat * 60 * 1000)
-                }
-
-                console.log('')
+                console.log(colors.red('Type a password'))
+                return process.exit()
             }
         })
-    })
+    } else {
+        init(email)
+    }
+})
+
+function init(email) {
+    console.log('')
+    console.log('Getting tree list...')
+    getTree(respository, file => file.path.indexOf('public') > -1).then(
+        list => {
+            checkFiles(list)
+        }
+    )
+
+    function checkFiles(list) {
+        console.log('Checking list: ' + new Date().toString())
+        let completed = 0
+        let incorrect = []
+        list = list.filter(item => extensions.includes(getExtension(item.path)))
+        list.forEach(item => {
+            const path = getPath(item)
+            getFile(path).then(body => {
+                completed += 1
+                const hash_github = item.sha
+                const hash_domain = shagit(body)
+                if (hash_domain === hash_github) {
+                    console.log(colors.green(`✔ ${hash_github} `) + path)
+                } else {
+                    incorrect.push(item)
+                    console.log(
+                        colors.red(`✘ ${hash_domain}`) +
+                            ` ${path}\n  ` +
+                            colors.red(hash_github)
+                    )
+                }
+
+                if (completed === list.length) {
+                    const total_incorrects = incorrect.length
+                    const total_list = list.length
+                    console.log(
+                        colors[total_incorrects === 0 ? 'green' : 'red'].bold(
+                            `  ${total_incorrects} fails`
+                        )
+                    )
+
+                    if (repeat) {
+                        if (total_incorrects > 0) {
+                            console.log('')
+                            console.log('Sending email...')
+                            const text = incorrect
+                                .map(item => `${item.sha} ${getPath(item)}`)
+                                .join('\n')
+
+                            server.send(
+                                {
+                                    subject:
+                                        'SOURCES IN COINFY DOES NOT MATCH WITH GITHUB!!',
+                                    text:
+                                        'Those files does no match with the github version:\n\n' +
+                                        text,
+                                    from: `you <${email}>`,
+                                    to: `you <${email}>`
+                                    // cc:      "else <else@your-email.com>",
+                                },
+                                function(err, message) {
+                                    if (err) {
+                                        console.log(colors.red(err.message))
+                                    } else
+                                        console.log(colors.green('Email sent!'))
+                                }
+                            )
+                        }
+
+                        setTimeout(() => {
+                            checkFiles(list)
+                        }, timeout_repeat * 60 * 1000)
+                    } else {
+                        process.exit()
+                    }
+                }
+            })
+        })
+    }
 }
 
 // UTILS FUNCTIONS
@@ -202,18 +211,14 @@ function validateEmail(email) {
     return re.test(email)
 }
 
-// prompt('Whats your name?', function (input) {
-//     console.log(input);
-//     process.exit();
-// });
-// function prompt(question, callback) {
-//     const stdin = process.stdin,
-//         stdout = process.stdout
+function prompt(question, callback) {
+    const stdin = process.stdin,
+        stdout = process.stdout
 
-//     stdin.resume()
-//     stdout.write(question)
+    stdin.resume()
+    stdout.write(question)
 
-//     stdin.once('data', data => {
-//         callback(data.toString().trim())
-//     })
-// }
+    stdin.once('data', data => {
+        callback(data.toString().trim())
+    })
+}
